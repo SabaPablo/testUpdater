73d976fe0dbbb389e8de25eec1c4e900ceec7702
Pablo Saba <sabaliauskaspablo@gmail.com>
2021-04-13 16:09:56 -0300

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const prueba = require("./prueba.json")
+
+app.get('/updates/au.json', function (req, res) {
+  res.send(
+    prueba
+  );
+});
+app.listen(3000, () => {
+ console.log("El servidor está inicializado en el puerto 3000");
+});
